34373ce3cfd03009f79b787cf0a329fab98385fc
kjh <softmaker005@gmail.com>
2024-03-27 16:38:02 +0900

    - 기존 테스트용 리포지토리 테스트 삭제 - MemberRepository 클래스 추가

--- src/main/java/jpa/commerce/repository/MemberRepository.java
+++ src/main/java/jpa/commerce/repository/MemberRepository.java
@@ -5,19 +5,35 @@ import jakarta.persistence.PersistenceContext;
 import jpa.commerce.domain.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MemberRepository {
 
     @PersistenceContext
     EntityManager em;
 
-    public Long save(Member member) {
+    // 멤버 저장
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    // 멤버 1명 조회
+    public Member findMemberById(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAllMembers() {
+        return em.createQuery(
+                "select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findMemberByName(String name) {
+        return em.createQuery(
+                "select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 
 }
