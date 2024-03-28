e6a49d5096c04aa50f1b22b0563fb890db8ac0a3
kjh <softmaker005@gmail.com>
2024-03-28 15:06:51 +0900

    - memberService 개발 완료 - memberServiceTest 개발 일부완료   -> 중복검증 예외처리 추후 추가필요

--- src/main/java/jpa/commerce/repository/MemberRepository.java
+++ src/main/java/jpa/commerce/repository/MemberRepository.java
@@ -18,17 +18,19 @@ public class MemberRepository {
         em.persist(member);
     }
 
-    // 멤버 1명 조회
+    // 멤버 1명 조회 - id값 사용
     public Member findMemberById(Long id) {
         return em.find(Member.class, id);
     }
 
+    // 모든 멤버 조회
     public List<Member> findAllMembers() {
         return em.createQuery(
                 "select m from Member m", Member.class)
                 .getResultList();
     }
 
+    // 멤버 조회 - 이름 사용
     public List<Member> findMemberByName(String name) {
         return em.createQuery(
                 "select m from Member m where m.name = :name", Member.class)
