a2cd3b6d3acdcd91a2fecc8ab8b54831417c0476
kjh <softmaker005@gmail.com>
2024-03-28 15:13:06 +0900

    * 파일 미첨부로 인한 재 commit - memberService 개발 완료 - memberServiceTest 개발 일부완료   -> 중복검증 예외처리 추후 추가필요

---
+++ src/main/java/jpa/commerce/service/MemberService.java
@@ -0,0 +1,50 @@
+package jpa.commerce.service;
+
+import jpa.commerce.domain.Member;
+import jpa.commerce.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor // @Autowired보다 이 어노테이션을 통한 생성자 주입이 더 효율적이다.
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    public Long regist(Member member) {
+
+        isDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void isDuplicateMember(Member member) {
+        List<Member> findMemberNames = memberRepository.findMemberByName(member.getName());
+        if (!findMemberNames.isEmpty()) { // 있는지 없는지 판단 로직은 리포지토리 sql 쿼리문에 있다.
+            throw new IllegalStateException("중복 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findAllMembers() {
+        return memberRepository.findAllMembers();
+    }
+
+    /**
+     * 개별 회원 조회 - id값 사용
+     */
+    public Member findMemberById(Long memberId) {
+        return memberRepository.findMemberById(memberId);
+    }
+
+}
---
+++ src/test/java/jpa/commerce/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpa.commerce.service;
+
+import jpa.commerce.domain.Member;
+import jpa.commerce.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+//import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    @Test
+    public void 테스트_회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("testName1");
+
+        //when
+        Long registedId = memberService.regist(member);
+
+        //then
+        //assertThat(registedId).isEqualTo(member.getId());
+        assertEquals(member.getId(), registedId);
+    }
+
+    @Test
+    public void 테스트_중복회원_검증() {
+        //given
+        Member member1 = new Member();
+        member1.setName("testName1");
+        Member member2 = new Member();
+        member2.setName("testName2");
+
+        //when
+        memberService.regist(member1);
+        memberService.regist(member2);
+
+        //then
+        fail("테스트 검증에 실패하고 예외가 발생해야 한다. 아직 커스텀 예외 정의하지 않음.");
+    }
+
+}
